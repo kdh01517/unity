fb5cd56b75d1d4a52a207318546980a9ca565f6f
DONGHYUN KIM <kdh01517@gmail.com>
2017-07-07 10:50:37 +0900

    This is the first version of team project.

---
+++ Assets/character/CharactorManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharactorManager : MonoBehaviour {
+
+    private GameObject snowObj;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    // 상태에 따라 이미지 변화하는 함수 혹은 씬을 변화
+    public void createSnow()
+    {
+        GameObject obj = Instantiate (snowObj, Vector3.zero, Quaternion.identity) as GameObject;
+
+        obj.transform.parent = GameObject.Find("snow").transform;
+        obj.transform.localPosition = new Vector3(-7.6294e-06f, 1.5259e-05f, 0.0f);
+        obj.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+
+        
+    }
+}
